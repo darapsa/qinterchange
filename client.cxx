3fdde5e115af1edf084a81ead04adbca49d39e88
ꦌ ꦫꦶ ꦏ꧀ꦦꦿ ꦧ ꦮ ꦑ ꦩ ꦭ꧀ <erik@darapsa.co.id>
2019-09-26 22:04:11 +0800

    All products related signal and slot

--- client.cxx
+++ client.cxx
@@ -13,6 +13,15 @@ namespace ICClient {
 		icclient_cleanup();
 	}
 
+	void productAll(icclient_catalog** catalogptr
+			, size_t (*callback)(void* contents
+				, size_t size, size_t nmemb
+				, void* userdata))
+	{
+		icclient_product_all(catalogptr, callback);
+		emit gotProductAll(*catalogptr);
+	}
+
 	void Client::logIn(QString const& username, QString const& password)
 	{
 		icclient_login(username.toLatin1().constData()
--- qicclient/client.hxx
+++ qicclient/client.hxx
@@ -2,6 +2,9 @@
 #define QRTCLIENT_CLIENT_HXX
 
 #include <QObject>
+#include <icclient/product.h>
+
+struct icclient_catalog;
 
 namespace ICClient {
 
@@ -13,11 +16,17 @@ namespace ICClient {
 			Client(char const* url, char const* certificate = nullptr);
 			~Client();
 
+			void productAll(icclient_catalog** catalogptr
+					, size_t (*callback)(void* contents
+						, size_t size, size_t nmemb
+						, void* userdata));
+
 		public slots:
 			void logIn(QString const& username, QString const& password);
 			void logOut();
 
 		signals:
+			void gotProductAll(icclient_catalog*);
 			void loggedIn(QString const& username);
 			void loggedOut();
 	};
