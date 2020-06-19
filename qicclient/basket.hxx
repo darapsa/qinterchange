3e66f47dc7bd6223fe39b67db5eeed5fb3fa6adc
ꦌ ꦫꦶ ꦏ꧀ꦦꦿ ꦧ ꦮ ꦑ ꦩ ꦭ꧀ <erik@darapsa.co.id>
2020-06-19 16:22:38 +0800

    Tidy up a bit

--- qicclient/basket.hxx
+++ qicclient/basket.hxx
@@ -45,9 +45,9 @@ namespace ICClient {
 				, m_totalCost{.0}
 			{}
 
-			QVariant data(const QModelIndex& index
-					, int role = Qt::DisplayRole
-					) const Q_DECL_OVERRIDE;
+			QVariant data(const QModelIndex& index,
+					int role = Qt::DisplayRole
+				     ) const Q_DECL_OVERRIDE;
 			int rowCount(QModelIndex const& parent
 					= QModelIndex()) const Q_DECL_OVERRIDE;
 
