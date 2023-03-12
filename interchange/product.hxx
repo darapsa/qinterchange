628fbf1304bff4de9be752d2f4be77d58a5ab689
ꦌꦫꦶꦏ꧀ꦦꦿꦧꦮꦑꦩꦭ꧀ <erik@darapsa.co.id>
2023-03-12 17:08:09 +0800

    Product is constructed using C struct copy

--- interchange/product.hxx
+++ interchange/product.hxx
@@ -23,30 +23,30 @@ namespace QInterchange {
 		};
 
 		Product() {}
-		Product(struct interchange_product* product) :
-			price{product->price},
-			weight{product->weight}
+		Product(struct interchange_product product) :
+			price{product.price},
+			weight{product.weight}
 		{
-			if (product->sku)
-				sku = QString{product->sku};
-			if (product->description)
-				description = QString{product->description};
-			if (product->title)
-				title = QString{product->title};
-			if (product->comment)
-				comment = QString{product->comment};
-			if (product->thumb)
-				thumb = QString{product->thumb};
-			if (product->image)
-				image = QString{product->image};
-			if (product->prod_group)
-				prodGroup = QString{product->prod_group};
-			if (product->category)
-				category = QString{product->category};
-			if (product->author)
-				author = QString{product->author};
-			if (product->crosssell) {
-				auto crosssell = product->crosssell;
+			if (product.sku)
+				sku = QString{product.sku};
+			if (product.description)
+				description = QString{product.description};
+			if (product.title)
+				title = QString{product.title};
+			if (product.comment)
+				comment = QString{product.comment};
+			if (product.thumb)
+				thumb = QString{product.thumb};
+			if (product.image)
+				image = QString{product.image};
+			if (product.prod_group)
+				prodGroup = QString{product.prod_group};
+			if (product.category)
+				category = QString{product.category};
+			if (product.author)
+				author = QString{product.author};
+			if (product.crosssell) {
+				auto crosssell = product.crosssell;
 				for (size_t i = 0; i < crosssell->length; i++)
 					crossSell << QString{crosssell->skus[i]};
 			}
